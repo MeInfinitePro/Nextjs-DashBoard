b7d1b543a060c668c79127681d15d72215a3743b
InfinitProgress <hayc888@qq.com>
2026-03-15 20:13:33 +0800

    'fonts'

--- app/ui/app/ui/dashboard/cards.tsx
+++ app/ui/app/ui/dashboard/cards.tsx
@@ -3,7 +3,8 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from "@heroicons/react/24/outline";
+} from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -36,7 +37,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: "invoices" | "customers" | "pending" | "collected";
+  type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
 
@@ -47,7 +48,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`
+        className={`${lusitana.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
